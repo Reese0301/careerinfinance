3c8be54ddb0a6d442e1df3d4e3412a42b2361f3c
Reese0301 <110365396+Reese0301@users.noreply.github.com>
2024-10-31 23:19:54 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import requests
 
-
 # Show title and description.
 st.title("💬 Career Advisor in Finance")
 st.write(
@@ -9,10 +8,8 @@ st.write(
     "To use this app, enter your query in the chat below."
 )
 
-
 # Set your Flowise API URL
 API_URL = "https://flowise-9kx9.onrender.com/api/v1/prediction/cef2a608-65a9-4813-a3a7-171a153c40b3"
-
 
 # Function to send queries to the API
 def query(payload):
@@ -22,11 +19,14 @@ def query(payload):
     else:
         return f"Error: {response.status_code}"
 
-
 # Initialize the session state for chat messages
 if "messages" not in st.session_state:
     st.session_state.messages = []
-
+    # Add a default welcome message from the assistant
+    st.session_state.messages.append({
+        "role": "assistant", 
+        "content": "Hello! I'm here to assist you with any finance and investment banking recruiting questions you may have. How can I help you today?"
+    })
 
 # Display existing chat messages with profile pictures
 for message in st.session_state.messages:
@@ -39,20 +39,17 @@ for message in st.session_state.messages:
         with st.chat_message("assistant", avatar="https://github.com/Reese0301/GIS-AI-Agent/blob/main/4322991.png?raw=true"):
             st.markdown(message["content"])
 
-
 # Chat input field for user to enter a message
 if prompt := st.chat_input("Ask your question here..."):
-
 
     # Store and display the user's message with the user avatar
     st.session_state.messages.append({"role": "user", "content": prompt})
     with st.chat_message("user", avatar="https://github.com/Reese0301/GIS-AI-Agent/blob/main/FoxUser.png?raw=true"):
         st.markdown(prompt)
 
-
     # Send the query to your custom API
     response_content = query({"question": prompt})
-   
+
     # Display the assistant's response with the assistant avatar
     with st.chat_message("assistant", avatar="https://github.com/Reese0301/GIS-AI-Agent/blob/main/4322991.png?raw=true"):
         st.markdown(response_content)
